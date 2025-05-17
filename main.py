52280d6d53003700360b293715e14fe73f9d6427
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2025-05-17 14:31:28 -0400

    read the contents of frankenstein and print the number of words

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath) -> str:
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    content = get_book_text("books/frankenstein.txt")
+    words = content.split()
+    num_words = len(words)
+    print(f"{num_words} words found in the document")
+
+main()
