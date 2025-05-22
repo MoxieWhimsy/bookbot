f70e6861f5c06389ad1201dc304f1a2eca3add68
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2025-05-22 15:40:40 -0400

    add get_letter_dict method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words,get_letter_dict
 
 def get_book_text(filepath) -> str:
     with open(filepath) as f:
@@ -9,5 +9,6 @@ def main():
     content = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(content)
     print(f"{num_words} words found in the document")
+    print(get_letter_dict(content))
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_num_words(content) -> int:
     words = content.split()
     num_words = len(words)
     return num_words
+
+def get_letter_dict(content) -> dict:
+    result = {}
+    for letter in content:
+        letter = letter.lower()
+        if letter not in result:
+            result[letter] = 1
+            continue
+        result[letter] += 1
+    return result
