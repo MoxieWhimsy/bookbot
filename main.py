d15c6cfdd66ca4a867797c9dc090043bbc756f56
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2025-05-30 19:12:08 -0400

    add get_sorted_letter_list function
    bonus: get_letter_dict function filters for alphabet letters

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from stats import get_num_words,get_letter_dict
+from stats import get_num_words,get_letter_dict,get_sorted_letter_list
 
 def get_book_text(filepath) -> str:
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-def main():
-    content = get_book_text("books/frankenstein.txt")
+def get_report(filepath) -> str:
+    content = get_book_text(filepath)
     num_words = get_num_words(content)
-    print(f"{num_words} words found in the document")
-    print(get_letter_dict(content))
+    letter_dict = get_letter_dict(content)
+    letter_list = get_sorted_letter_list(letter_dict)
+    result = "============ BOOKBOT ============\n"
+    result += f"Analyzing book found at {filepath}...\n"
+    result += "----------- Word Count ----------\n"
+    result += f"Found {num_words} total words\n"
+    result += "--------- Character Count -------\n"
+    for pair in letter_list:
+        result += f"{pair["char"]}: {pair["num"]}\n"
+    result += "============= END ==============="
+    return result
+
+def main():
+    print(get_report("books/frankenstein.txt"))
 
 main()
--- stats.py
+++ stats.py
@@ -7,8 +7,22 @@ def get_letter_dict(content) -> dict:
     result = {}
     for letter in content:
         letter = letter.lower()
+        if not letter.isalpha():
+            continue
         if letter not in result:
             result[letter] = 1
             continue
         result[letter] += 1
     return result
+
+def get_sorted_letter_list(letter_dict: dict) -> list:
+    def get_count(e):
+        return e["num"]
+    letter_list = []
+    for letter in letter_dict:
+        pair = dict()
+        pair["char"] = letter
+        pair["num"] = letter_dict[letter]
+        letter_list.append(pair)
+    letter_list.sort(key=get_count, reverse=True)
+    return letter_list
